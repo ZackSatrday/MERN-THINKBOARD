0be88cd2643084c6e9a0d98afffa5185a279d51e
ZackSatrday <sushant.kr398@gmail.com>
2025-07-08 20:43:48 +0530

    miner change

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,10 +11,7 @@ const PORT = process.env.PORT || 5001;
 
 //middleware 
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173', // Adjust this to your frontend URL
-    credentials: true, // Allow credentials if needed
-}))
+app.use(cors())
 
 app.use('/api/notes', notesRoutes);
 
